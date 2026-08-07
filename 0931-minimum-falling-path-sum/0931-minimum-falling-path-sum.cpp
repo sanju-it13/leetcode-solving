221151f31fb988465b29e3ab9c9ab2e9db80ef56
Sanju Maity <sanjumaity099@gmail.com>
2026-08-07 23:43:26 +0530

    Time: 0 ms (100%), Space: 14.4 MB (88.04%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
-  
-  int pathsum(int i, int j, vector<vector<int>>& matrix,vector<vector<int>>&dp) {
-    int n = matrix.size();
-    if (j<0 || j>n-1)
-      return INT_MAX;
-
-    if (i==n-1)
-      return matrix[i][j];  
-
-    if (dp[i][j] != INT_MAX)
-       return dp[i][j];
-
-    // 3 case
-
-    int down = pathsum(i+1,j,matrix,dp);
-
-    int left = pathsum(i+1,j-1,matrix,dp);
-
-    int right = pathsum(i+1,j+1,matrix,dp);
-
-
-    return dp[i][j]= matrix[i][j]+min({down,left,right});      
-
-  }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         
         int n = matrix.size();
-        int result = INT_MAX;
-        vector<vector<int>>dp(n,vector<int>(n,INT_MAX));
 
-        for(int j=0; j<n;j++){
-            result = min(result, pathsum(0,j,matrix,dp)); // we can choose any element of first row.. 
+        for(int i=n-2; i>=0;i--){
+           for(int j = n-1;j>=0;j--){
+
+            // case 1 (row + 1, col)
+            int minchoice = matrix[i+1][j];
+
+            // case 2  (row + 1, col - 1)
+            if(j>0)
+             minchoice = min(minchoice , matrix[i+1][j-1]);
+
+            // case 3 (row + 1, col + 1)
+            if (j < n-1)
+              minchoice = min(minchoice , matrix[i+1][j+1]);
+
+            matrix[i][j]+=minchoice;  
+           }
+        }
+
+        int result = INT_MAX;
+
+        for(int j=0;j<n;j++){
+            result = min(result,matrix[0][j]);
         }
 
         return result;
-    }   
+    }
 };
