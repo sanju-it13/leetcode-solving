4f90e2dc76699423a94ec432518b3d0939247cbc
Sanju Maity <sanjumaity099@gmail.com>
2026-08-08 14:51:58 +0530

    Time: 45 ms (55.32%), Space: 97 MB (90.43%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -12,8 +12,8 @@ public:
         for(int i=n-1, j= m-1 ; i>=0 && j>=0; i--){
 
             if(word1[i] == word2[j]){
-              right[j]=i;
-              j--;
+              right[j--]=i;
+              
             }
         }
 
@@ -23,16 +23,15 @@ public:
       int j =0;
       for(int i=0; i<n && j<m ; i++){
         if(word1[i]==word2[j]){
-             answer[j]=i;
-             j++;
+             answer[j++]=i;
+             
         }
         else if(flag && (j==m-1 || i<right[j+1])){
             // if i =2.. then the value store in right[] must be after index 2.. otherwise not strictly increasing or lexicographically order like 1,2,3,4...
              
-             answer[j]=i;
-             j++;
+             answer[j++]=i;
+             
              flag = false;
-
         }
 
         if(j==m) return answer;
