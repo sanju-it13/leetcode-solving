8231cd8fbf619598d223ff3351cf760f4b36153e
Sanju Maity <sanjumaity099@gmail.com>
2026-08-09 22:05:41 +0530

    Time: 31 ms (21.81%), Space: 13.5 MB (64.48%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -6,17 +6,16 @@ public:
         int n = piles.size();
          
         vector<vector<int>>dp(n+1,vector<int>(n+1,0));  
+        vector<int>suffix(n+1,0);
+        for(int i=n-1;i>=0;i--){
+            suffix[i]=suffix[i+1]+piles[i];
+        }
+
 
         for(int index=n-1;index>=0;index--){
-    
-          // total remaining stone 
-           int total =0;
-           for(int i=index;i<n;i++){
-            total+=piles[i];
-           }
 
         for(int M=1;M<=n;M++){
-
+            int total = suffix[index];
              int answer=0;
 
         for(int X=1;X<=2*M;X++){
