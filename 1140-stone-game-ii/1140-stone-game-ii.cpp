d746eee9e3a849f2d62afdb0742ae1b488bbd84e
Sanju Maity <sanjumaity099@gmail.com>
2026-08-09 21:58:32 +0530

    Time: 34 ms (21.23%), Space: 13.5 MB (64.48%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.cpp
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
 
-    int stoneCollect(int index , int M, vector<int>&piles,vector<vector<int>>&dp){
-        int n=piles.size();
-       
-        if(index>=n) return 0;
+    int stoneGameII(vector<int>& piles) {
 
-        if(dp[index][M]!=-1) return dp[index][M];
+        int n = piles.size();
+         
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));  
 
-         // total remaining stone 
-        int total =0;
-        for(int i=index;i<n;i++){
+        for(int index=n-1;index>=0;index--){
+    
+          // total remaining stone 
+           int total =0;
+           for(int i=index;i<n;i++){
             total+=piles[i];
-        }
+           }
 
-        int answer=0;
+        for(int M=1;M<=n;M++){
+
+             int answer=0;
+
         for(int X=1;X<=2*M;X++){
             
             // from first index to X -> taken by Alice.. So Bob can take from (index+X)
             // index+x can go upto piles.size();
-            if(index+X > piles.size()) break;
+            if(index+X >n) break;
 
             int newM =max(X,M);
 
-            int Bob = stoneCollect(index+X,newM,piles,dp);
+            int Bob = dp[index+X][newM];
 
             // Alice collected Stone = Total - Bob collected stone
             answer = max (answer, total- Bob);
         }
-          
-        return dp[index][M]=answer;
-    }
-    int stoneGameII(vector<int>& piles) {
-        int n = piles.size();
+        dp[index][M]=answer;
+        }
+        }
 
-        // X,M changing part.. 2D dp
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));  
-       
-        return stoneCollect(0,1,piles,dp);
-        
+       return dp[0][1]; 
     }
 };
