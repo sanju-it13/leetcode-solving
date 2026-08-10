d29e8a9d1e286dfa664536c44e225180e649eb6e
Sanju Maity <sanjumaity099@gmail.com>
2026-08-10 20:35:09 +0530

    Time: 31 ms (81.44%), Space: 8.8 MB (90.65%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
 
-    bool answer(int n, vector<int>&dp){
-
-        if(n==0) 
-         return false; // whoever turn(alice/bob) it was losses game..
-
-        if(dp[n]!=-1)
-          return dp[n];
-
-        // alice first turn.. 
-        // alice choose any i^2.. (1,4,9,16...).. i^2 <= n..
-
-        // now Bob turn.. 
-
-        //(n - i^2)..  if answer(n-i^2) is true then Bob win.. means alice lose.. but we have to return true only when Alice win..
-         //  if answer(n-i^2) is equal to false means Bob lost.. Alice win..
-
-        for(int i=1; i*i <= n; i++ ){
-          
-           if(answer(n-i*i,dp) == false)
-              return dp[n]=true;
-
-        }
-        return dp[n]=false;  // alice could never win.
-    
-    }
-
     bool winnerSquareGame(int n) {
-        vector<int>dp(n+1,-1);
-        return answer(n,dp);
+       
+        vector<bool>dp(n+1,false);
+
+         // dp[0]=false; already we start dp all false.
+     for(int i=1;i<n+1;i++){
+
+         for(int k=1; k*k <= i; k++ ){
+          
+           if(dp[i - k*k] == false)
+            {
+                dp[i]=true;
+                break;
+            }
+              
+        }
+     }
+        return dp[n];
     }
 };
