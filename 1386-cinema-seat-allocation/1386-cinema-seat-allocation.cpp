ecb586f842de0fcddc4bd31bb12e87e9794a1ef8
Sanju Maity <sanjumaity099@gmail.com>
2026-08-20 00:20:48 +0530

    Time: 24 ms (69.93%), Space: 60.2 MB (68.07%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -11,29 +11,33 @@ public:
           -> if a row has no occupied seat then for this row .. we will book 2 group seat(A,C)
           -> otherwise we will check each group with occupied seat. */
 
-        unordered_map<int,unordered_set<int>> x;
+        unordered_map<int,int> x;
         // row no and seats in each row booked.
 
         for(auto& reserved : reservedSeats){   
          //reservedSeats = [[1,2],[1,3],[1,8],[2,6],[3,1],[3,10]]
           int row =reserved[0];
           int seat =reserved[1];
-           // insert it in map
-           x[row].insert(seat);
-           
+    // doing OR operation and left shift.. [1,2],[1,8] -> 0010000010
+           x[row] = (x[row] | (1 << seat));   
         }
          int empty_seat= n-x.size();
         int result = empty_seat * 2;  // 2 group book for all empty row
-          
+
+         // Group A {2,3,4,5} -> 0000011110
+         // Group B {4,5,6,7} -> 0001111000
+         // Group C {6,7,8,9} -> 0111100000
+
+         int maskA=(1<<2)|(1<<3)|(1<<4)|(1<<5);
+         int maskB=(1<<4)|(1<<5)|(1<<6)|(1<<7);
+         int maskC=(1<<6)|(1<<7)|(1<<8)|(1<<9); 
+
         for(auto& [row , occupied_seat] : x){
                
-          auto isAvailable=[&](int seat){
-            return occupied_seat.find(seat) == occupied_seat.end();
-          };
 
-         bool GroupA= isAvailable(2) && isAvailable(3) && isAvailable(4) && isAvailable(5);
-         bool GroupB= isAvailable(4) && isAvailable(5) && isAvailable(6) && isAvailable(7);
-         bool GroupC= isAvailable(6) && isAvailable(7) && isAvailable(8) && isAvailable(9);
+         bool GroupA= (occupied_seat & maskA) ==0;
+         bool GroupB= (occupied_seat & maskB) ==0;
+         bool GroupC= (occupied_seat & maskC) ==0;
 
          if(GroupA && GroupC)
            result+=2;
