6570530c4774a653a9f0d5e3213313cbf52fc037
Sanju Maity <sanjumaity099@gmail.com>
2026-08-30 00:40:28 +0530

    Time: 0 ms (100%), Space: 19.5 MB (65.15%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -3,13 +3,11 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
        // two pointer
        int i=0,j=nums.size()-1;
-       vector<int>a;
+      
        while(i<j){
         int ans=nums[i]+nums[j];
         
         if(ans == target){
-            a.push_back(i+1);
-            a.push_back(j+1);
            break;
         }
 
@@ -19,7 +17,7 @@ public:
             i++;
         } 
        }
-       return a;
+       return {i+1,j+1};
         
     }
 };
