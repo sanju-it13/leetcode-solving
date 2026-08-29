fd3f4a4c9a0f7fdb8f0103e8f0a527adadd44c09
Sanju Maity <sanjumaity099@gmail.com>
2026-08-30 00:56:42 +0530

    Time: 1 ms (77.12%), Space: 15.1 MB (12.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        unordered_map<int, int>arr;
+        int n = nums.size();
+
+        for (int first = 0; first < n; first++) {
+            int second = target - nums[first]; 
+            if (arr.count(second)) {
+                return {arr[second], first}; // try to find second is available or not
             }
+            arr[nums[first]] = first; // add the current no and its index to hash map
         }
+
         return {};
     }
 };
