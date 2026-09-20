a24fdca4a60d2f61a1c64e7057ed9931a6b321cb
Sanju Maity <sanjumaity099@gmail.com>
2026-09-20 23:00:47 +0530

    Time: 0 ms (100%), Space: 9.9 MB (8.02%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int reverseDegree(string s) {
         int sum=0;
-        for(int i=0;i<s.length();i++){
-            sum+=('z'-s[i]+1) * (i+1);
-           
-        }
+        for(int i=0;i<s.length();i++)
+         sum+=(26-(s[i]-'a'))*(i+1);
+         // s[i]=a  ->  26- (a - a) =26.. a=26
+         // s[i]=z  ->  26 - (z-a) = 26 - 25 = 1
+        
         return sum;
     }
 };
