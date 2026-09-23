041f9dbb74b644a8ff79b41f6ffa130e6305f30b
Sanju Maity <sanjumaity099@gmail.com>
2026-09-24 00:51:34 +0530

    Time: 0 ms (100%), Space: 102.3 MB (91.82%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -2,30 +2,28 @@ class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
         int total=0;
-        for(int i=0;i<nums.size();i++){
-            total+=nums[i];
-        }
+        for(int num : nums)
+           total+=num;
         int target = total - x;
         if (target==0) return nums.size();
         if(target<0) return -1;
-        unordered_map<int,int>mp;
-        mp[0]=-1;
-        int prefixsum=0;
+        int Currsum=0;
         int maxLen=-1;
-        for(int i=0;i<nums.size();i++){
-            prefixsum+=nums[i];
-            int required = prefixsum-target;
-
-            if(mp.find(required) != mp.end())
-                maxLen=max(maxLen, i-mp[required]);
-            
-            if(mp.find(prefixsum)==mp.end())
-               mp[prefixsum]=i;
+        int left=0;
+        for(int right=0;right<nums.size();right++){
+            Currsum+=nums[right];
+              // shrink window if Currsum > target
+             while(Currsum > target && left<=right){
+                Currsum-=nums[left];
+                left++;
+             }
+            if(Currsum == target){
+                int Currlength=right-left+1;
+                maxLen=max(maxLen,Currlength);
+            }
         }
 
-        if(maxLen==-1) return -1;
-
-        return nums.size()-maxLen;
+        return (maxLen==-1) ? -1 : (nums.size()-maxLen);
         
     }
 };
